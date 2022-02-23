70938bfbd131bd3773fceffa589e736ed6d06470
kamilFitrzyk <kamilfitrzyk89@gmail.com>
2022-02-23 13:57:52 +0100

    added visit, update owner with pet relations

--- petclinic-data/src/main/java/springframework/petclinic/model/Owner.java
+++ petclinic-data/src/main/java/springframework/petclinic/model/Owner.java
@@ -1,6 +1,19 @@
 package springframework.petclinic.model;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 
 
 }
