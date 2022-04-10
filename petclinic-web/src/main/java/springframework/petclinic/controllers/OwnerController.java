cfaade0a3c75fce32b4386d34e1dbbf959d0356c
kamilFitrzyk <kamilfitrzyk89@gmail.com>
2022-04-10 21:42:05 +0200

    create pet not working

--- petclinic-web/src/main/java/springframework/petclinic/controllers/OwnerController.java
+++ petclinic-web/src/main/java/springframework/petclinic/controllers/OwnerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import springframework.petclinic.model.Owner;
 import springframework.petclinic.service.OwnerService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/owners")
@@ -103,7 +104,7 @@ public class OwnerController {
     }
 
     @PostMapping("/{ownerId}/edit")
-    public String processUpdateOwnerForm(@Validated Owner owner, BindingResult result, @PathVariable Long ownerId) {
+    public String processUpdateOwnerForm(@Valid Owner owner, BindingResult result, @PathVariable Long ownerId) {
         if(result.hasErrors()) {
             return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
         } else {
